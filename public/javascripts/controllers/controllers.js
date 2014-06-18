10dcb9c0b246790849397a39e79c21bf0f33639a
xiaokchengkun <aegean.ck@gmail.com>
2014-06-18 23:24:17 +0800

    notification

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -91,33 +91,28 @@ controllers.controller("reply365Controller", ["$scope", "reply365Service",
 		};
 
 		$scope.notice = function(){
+			var title = "有新信息了!!!";
+			var options = {
+				dir: "ltr",
+				body: $scope.target.content || 0,
+				icon: "http://timg.cmwb.com/face/temp/middlehead.jpg"
+			};
 			if (!("Notification" in window)) {
-		    alert("This browser does not support desktop notification");
-		  }
-
-		  // Let's check if the user is okay to get some notification
-		  else if (Notification.permission === "granted") {
-		    // If it's okay let's create a notification
-		    var notification = new Notification("Hi there!");
-		  }
-
-		  // Otherwise, we need to ask the user for permission
-		  // Note, Chrome does not implement the permission static property
-		  // So we have to check for NOT 'denied' instead of 'default'
-		  else if (Notification.permission !== 'denied') {
-		    Notification.requestPermission(function (permission) {
-
-		      // Whatever the user answers, we make sure we store the information
-		      if(!('permission' in Notification)) {
-		        Notification.permission = permission;
-		      }
-
-		      // If the user is okay, let's create a notification
-		      if (permission === "granted") {
-		        var notification = new Notification("Hi there!");
-		      }
-		    });
-		  }
+				alert(title);
+			}
+			else if (Notification.permission === "granted") {
+				var notification = new Notification(title, options);
+			}
+			else if (Notification.permission !== 'denied') {
+				Notification.requestPermission(function (permission) {
+					if(!('permission' in Notification)) {
+						Notification.permission = permission;
+					}
+					if (permission === "granted") {
+						var notification = new Notification();
+					}
+				});
+			}
 		};
 	}
 ]);
